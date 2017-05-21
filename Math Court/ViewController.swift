12593ebbeb6bdd2d27fe13d1cf31abb72c42e8d9
Shyam Lingam <Shyam@shyams-air.gateway>
2017-05-21 14:12:28 +1000

    Adding Background music on/off switch. Test Switch works fine.

--- Math Court/ViewController.swift
+++ Math Court/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        player.stop()
+    }
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // add a comment
+       
+        do
+        {
+            let audioPath = Bundle.main.path(forResource: "b song", ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+        }
+        catch
+        {
+            //PROCESS ERROR
+        }
+        
     }
-
+    
+        //Code Step for Background Music
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
